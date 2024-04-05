492bfdb0346d6f97e8521b35cde62074c3fdf9ee
Matthew Morales <mtthwmrls9@gmail.com>
2024-04-04 18:47:55 -0600

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -54,6 +54,184 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+void config_red () {
+	GPIOC->MODER &= ~(GPIO_MODER_MODER6_Msk);
+	GPIOC->MODER |= GPIO_MODER_MODER6_0;
+	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_6);
+	GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR6_Msk);
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR6_Msk);
+	GPIOC->ODR &= ~GPIO_ODR_6;
+}
+void toggle_red (char mode) {
+	switch (mode) {
+		case 2:
+			GPIOC->ODR ^= GPIO_ODR_6;
+			break;
+		case 1:
+			GPIOC->ODR |= GPIO_ODR_6;
+			break;
+		case 0:
+			GPIOC->ODR &= ~GPIO_ODR_6;
+			break;
+	}
+}
+void config_blue () {
+	GPIOC->MODER &= ~(GPIO_MODER_MODER7_Msk);
+	GPIOC->MODER |= GPIO_MODER_MODER7_0;
+	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_7);
+	GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR7_Msk);
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR7_Msk);
+	GPIOC->ODR &= ~GPIO_ODR_7;
+}
+void toggle_blue (char mode) {
+	switch (mode) {
+		case 2:
+			GPIOC->ODR ^= GPIO_ODR_7;
+			break;
+		case 1:
+			GPIOC->ODR |= GPIO_ODR_7;
+			break;
+		case 0:
+			GPIOC->ODR &= ~GPIO_ODR_7;
+			break;
+	}
+}
+void config_orange () {
+	GPIOC->MODER &= ~(GPIO_MODER_MODER8_Msk);
+	GPIOC->MODER |= GPIO_MODER_MODER8_0;
+	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_8);
+	GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR8_Msk);
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR8_Msk);
+	GPIOC->ODR &= ~GPIO_ODR_8;
+}
+void toggle_orange (char mode) {
+	switch (mode) {
+		case 2:
+			GPIOC->ODR ^= GPIO_ODR_8;
+			break;
+		case 1:
+			GPIOC->ODR |= GPIO_ODR_8;
+			break;
+		case 0:
+			GPIOC->ODR &= ~GPIO_ODR_8;
+			break;
+	}
+}
+void config_green () {
+	GPIOC->MODER &= ~(GPIO_MODER_MODER9_Msk);
+	GPIOC->MODER |= GPIO_MODER_MODER9_0;
+	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_9);
+	GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR9_Msk);
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR9_Msk);
+	GPIOC->ODR &= ~GPIO_ODR_9;
+}
+void toggle_green (char mode) {
+	switch (mode) {
+		case 2:
+			GPIOC->ODR ^= GPIO_ODR_9;
+			break;
+		case 1:
+			GPIOC->ODR |= GPIO_ODR_9;
+			break;
+		case 0:
+			GPIOC->ODR &= ~GPIO_ODR_9;
+			break;
+	}
+}
+
+enum PuzzleStateType {
+	Puzzle1,
+	Puzzle2,
+	Puzzle3,
+	GameEnd
+};
+
+// returns true when puzzle is solved
+int doPuzzle1() {
+	if (ADC1->DR > 5) {
+		toggle_red(1);
+	} else {
+		toggle_red(0);
+	}
+	return 0;
+}
+
+int doPuzzle2() {
+	return 0;
+}
+
+int doPuzzle3() {
+	return 0;
+}
+
+void doGameEnd() {
+	
+}
+
+void playFanfare() {
+	
+}
+
+void init() {
+	
+	// none of this works yet
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+
+	//enable clock to timer 3
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+	
+	// set PWM mode 1 on channel 2
+	TIM3->CCMR1 &= ~(TIM_CCMR1_OC2M_Msk); // clear
+	TIM3->CCMR1 |= (0x6 << TIM_CCMR1_OC2M_Pos); // PWM mode 1
+	
+	//enable
+	TIM3->CCER |= TIM_CCER_CC2E;
+	
+	TIM3->PSC = (short)99; // divide clock to 8000 khz
+	TIM3->ARR = 100;
+	
+	// duty cycle
+	TIM3->CCR2 = 20;
+	
+	//preload
+	TIM3->CCMR1 |= TIM_CCMR1_OC2PE;
+	
+	// alternate function
+	GPIOC->AFR[0] &= ~GPIO_AFRL_AFRL7_Msk;
+	
+}
+
+void config_adc () {
+	// Set PA0 to analog mode
+	GPIOA->MODER |= 3 << GPIO_MODER_MODER0_Pos;
+	// Enable the clock to the ADC
+	RCC->APB2ENR |= RCC_APB2ENR_ADCEN;
+	// Set ADC1 to 8 bit resolution, continuous conversion mode, hardware triggers disabled.
+	ADC1->CFGR1 |= 2 << ADC_CFGR1_RES_Pos;
+	ADC1->CFGR1 |= ADC_CFGR1_CONT;
+	ADC1->CFGR1 &= ~ADC_CFGR1_ALIGN_Msk;
+	// Set ADC1 to use channel 10 (ADC_IN10 additional function)
+	ADC1->CHSELR |= ADC_CHSELR_CHSEL10;
+	// Start calibration
+	ADC1->CR |= ADC_CR_ADCAL;
+		
+	// Wait until the calibration bit is reset.
+	while (ADC1->CR & ADC_CR_ADCAL_Msk) {
+		HAL_Delay(1);
+	}
+	
+	// Enable the ADC
+	ADC1->CR |= ADC_CR_ADEN;
+	
+	// Wait until the ADC is ready
+	while (!(ADC1->ISR & ADC_ISR_ADRDY)) {
+		HAL_Delay(1);
+	}
+		
+	// Signal that we are ready for conversion
+	ADC1->CR |= ADC_CR_ADSTART;
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -72,6 +250,12 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
+	
+	// Enable the RCC clock to GPIOA
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	
+	config_red();
+	config_adc();
 
   /* USER CODE END Init */
 
@@ -85,6 +269,12 @@ int main(void)
   /* Initialize all configured peripherals */
   /* USER CODE BEGIN 2 */
 
+	
+	
+	
+	enum PuzzleStateType mainState = Puzzle1;
+	
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -92,6 +282,34 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+		
+		switch (mainState) {
+			case Puzzle1:
+				if (doPuzzle1()) {
+					playFanfare();
+					mainState = Puzzle2;
+				}
+				break;
+			
+			case Puzzle2:
+				if (doPuzzle2()) {
+					playFanfare();
+					mainState = Puzzle3;
+				}
+				break;
+				
+			case Puzzle3:
+				if (doPuzzle3()) {
+					playFanfare();
+					mainState = GameEnd;
+				}
+				break;
+			
+			case GameEnd:
+				doGameEnd();
+		}
+				
+		HAL_Delay(20);
 
     /* USER CODE BEGIN 3 */
   }
